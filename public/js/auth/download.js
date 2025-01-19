fd41ca9ce46d60612fcc1518754c53c2e1d1eade
Joe <joemunene91@gmail.com>
2025-01-18 23:54:04 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -131,14 +131,14 @@ auth.onAuthStateChanged(user => {
 						Verify your email inbox,  <br> Check the spam - folder.     <hr class="hr15-top"> `;
 					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
 				} else { 
-					setTimeout(() => { generatePDF(); }, 7500);
+					setTimeout(() => { generatePDF(); }, 7000);
 					var shortCutFunction = 'success';  var msg = ` 
 						${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 						Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
 					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 				} });
 			} else {
-				setTimeout(() => { window.location.assign('home'); }, 7500);
+				setTimeout(() => { window.location.assign('home'); }, 7000);
 				var shortCutFunction = 'success';  var msg = ` 
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.      <hr class="to-hr hr15-top"> 
 						Logins can be sent as a <br> .PDF file or via EMAIL.          <hr class="hr15-top"> `;
@@ -154,7 +154,7 @@ auth.onAuthStateChanged(user => {
 				}
 			});
 
-			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
+			setTimeout(() => { $('#exampleModal').modal('hide'); }, 4000);
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
