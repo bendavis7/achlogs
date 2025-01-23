99a265f1f31350c1f19cc0dae7d029914b4172e5
Joe <joemunene91@gmail.com>
2025-01-23 03:08:00 -0800

    Updates

--- js/auth/download.js
+++ js/auth/download.js
@@ -73,16 +73,7 @@ auth.onAuthStateChanged(user => {
 			thePerson = `<hr class="hr-2"> ${theaddress}.`;
 			jinaHolder.value = theaddress;
 			theGuy = user.email;
-
-			vpnButn.addEventListener('click', () => {
-				setTimeout(() => { generatePDF(); }, 2000);
-			});
-			vpnButn.innerHTML = `
-				.PDF File <img src="img/partners/pdf.png">`;
 		} else {
-			vpnButn.addEventListener('click', () => {
-				setTimeout(() => { window.location.assign('home'); }, 2000);
-			});
 			if (window.innerWidth < 1082) { 
 				thePerson = `<hr class="hr-2"> ${Device} <br> ${citiZ} `;
 			} else { 
@@ -138,7 +129,7 @@ auth.onAuthStateChanged(user => {
 							Verify your email inbox,  <br> Check the spam - folder.     <hr class="hr15-top"> `;
 						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
 					} else { 
-						setTimeout(() => { generatePDF(); }, 7000);
+						setTimeout(() => { generatePDF(); }, 7500);
 						var shortCutFunction = 'success';  var msg = ` 
 							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 							Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
@@ -146,23 +137,11 @@ auth.onAuthStateChanged(user => {
 					} 
 				});
 			} else {
-				var docRef = db.collection("users").doc(theGuy);
-				docRef.get().then((doc) => { 
-					var eData = JSON.parse(JSON.stringify(doc.data()));
-					if(!eData.download) { 
-						setTimeout(() => { generatePDF(); }, 7000);
-						var shortCutFunction = 'success';  var msg = ` 
-							${toastbtci} BTC not detected <br> Send exactly $${toastzi}.      <hr class="to-hr hr15-top"> 
-								Logins can be sent as a <br> .PDF file or via EMAIL..         <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
-					} else {
-						setTimeout(() => { window.location.assign('home'); }, 7000);
-						var shortCutFunction = 'success';  var msg = ` 
-							${toastbtci} BTC not detected <br> Send exactly $${toastzi}.      <hr class="to-hr hr15-top"> 
-								Logins can be sent as a <br> .PDF file or via EMAIL..         <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
-					}
-				});
+				setTimeout(() => { generatePDF(); }, 7500);
+				var shortCutFunction = 'success';  var msg = ` 
+					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.      <hr class="to-hr hr15-top"> 
+						Logins can be sent as a <br> .PDF file or via EMAIL..         <hr class="hr15-top"> `;
+				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 			}
 
 			var docRef = db.collection("users").doc(theGuy);
@@ -178,6 +157,10 @@ auth.onAuthStateChanged(user => {
 		});
 	}
 	document.getElementById('monez').addEventListener('click', signUpFunction);
+
+	vpnButn.addEventListener('click', () => {
+		setTimeout(() => { generatePDF(); }, 2000);
+	});
 
 	function generatePDF() {
 		var pdfObject = jsPDFInvoiceTemplate.default(props);
