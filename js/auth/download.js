1e4069da63fb59727d66b292af6dbfb84e2b33a5
Joe <joemunene91@gmail.com>
2025-01-24 07:15:54 -0800

    Updates

--- js/auth/download.js
+++ js/auth/download.js
@@ -117,15 +117,15 @@ auth.onAuthStateChanged(user => {
 					if (!(doc.exists)) { 
 						auth.currentUser.sendEmailVerification(); 
 						var shortCutFunction = 'success'; var msg = ` 
-							Bank logs will be sent to <br> ${user.email}.               <hr class="to-hr hr15-top"> 
+							Logins will be sent to.. <br> ${user.email}                 <hr class="to-hr hr15-top"> 
 							Verify your email inbox,  <br> Check the spam - folder.     <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
 					} else { 
-						setTimeout(() => { generatePDF(); }, 7500);
+						setTimeout(() => { generatePDF(); }, 8000);
 						var shortCutFunction = 'success';  var msg = ` 
 							${toastbtci} BTC not detected <br> Send exactly $${toastzi}. <hr class="to-hr hr15-top"> 
 							Bank logs will be sent to <br> ${user.email}.                <hr class="hr15-top"> `;
-						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+						toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 					} 
 				});
 			} 
