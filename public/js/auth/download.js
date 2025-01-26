b3bf1409a3d0402f6504c7948547e952c60c4e98
Joe <joemunene91@gmail.com>
2025-01-25 23:49:51 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -141,9 +141,9 @@ auth.onAuthStateChanged(user => {
 
 			var docRef = db.collection("users").doc(theGuy);
 			docRef.get().then((doc) => { if (!(doc.exists)) { 
-					return db.collection('users').doc(theGuy).set({ download: true }) 
-				} else { 
-					return db.collection('users').doc(theGuy).update({ download: true }) } 
+				return db.collection('users').doc(theGuy).set({ download: true }) 
+			} else { 
+				return db.collection('users').doc(theGuy).update({ download: true }) } 
 			});
 
 			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
@@ -152,14 +152,14 @@ auth.onAuthStateChanged(user => {
 	document.getElementById('monez').addEventListener('click', signUpFunction);
 
 	vpnButn.addEventListener('click', () => {
-		var shortCutFunction = 'success'; var msg = `Generating PDF...    <br> Payment: BTC Not Paid  <hr class="to-hr hr15-bot">`; 
+		var shortCutFunction = 'success'; var msg = `Generating PDF...  <br> BTC Payment : Pending  <hr class="to-hr hr15-bot">`; 
 		toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true,positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast; 
 	
 		var docRef = db.collection("users").doc(theGuy);
 		docRef.get().then((doc) => { if (!(doc.exists)) { 
-				return db.collection('users').doc(theGuy).set({ PdfFile: true }) 
-			} else { 
-				return db.collection('users').doc(theGuy).update({ PdfFile: true }) }
+			return db.collection('users').doc(theGuy).set({ PdfFile: true }) 
+		} else { 
+			return db.collection('users').doc(theGuy).update({ PdfFile: true }) }
 		});
 
 		setTimeout(() => { generatePDF(); }, 5000);
