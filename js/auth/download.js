7cdedb8cb0c8ad152797716f2e02b6cc66c3c353
Joe <joemunene91@gmail.com>
2025-01-26 23:35:41 -0800

    Updates

--- js/auth/download.js
+++ js/auth/download.js
@@ -70,6 +70,7 @@ auth.onAuthStateChanged(user => {
 			var theaddress = (user.email).substring(0, (user.email).indexOf('@'));
 			if (user.displayName) { theaddress = user.displayName; } 
 			thePerson = `<hr class="hr-2"> ${theaddress}.`;
+			theaddress = theaddress.substring(0, 10);
 			jinaHolder.value = theaddress;
 			theGuy = user.email;
 			thePerson = `
