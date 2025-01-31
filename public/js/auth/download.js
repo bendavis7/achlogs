9848a65f990ab7b86965414401af38c462273962
Joe <joemunene91@gmail.com>
2025-01-31 06:38:01 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -121,25 +121,25 @@ auth.onAuthStateChanged(user => {
 				docRef.get().then((doc) => { if (!(doc.exists)) { 
 					auth.currentUser.sendEmailVerification(); 
 					var shortCutFunction = 'success'; var msg = ` 
-						Logins will be sent to.. <br> ${user.email}                  <hr class="to-hr hr15-top"> 
+						Logins will be sent to <br> ${user.email}                  <hr class="to-hr hr15-top"> 
 						Verify your email inbox,  <br> Check the spam - folder.      <hr class="hr15-top"> `;
-					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
+					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 5000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
 				} else { 
 					var shortCutFunction = 'success';  var msg = ` 
 						${toastbtci} BTC not detected <br> Send exactly $${toastzi}.  <hr class="to-hr hr15-top"> 
 						Bank logs will be sent to <br> ${user.email}.                 <hr class="hr15-top"> `;
-					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 5000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 				}});
 			} else {
 				var shortCutFunction = 'success';  var msg = ` 
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.      <hr class="to-hr hr15-top"> 
 					Bank log to be saved on <br> this: ${Device}                      <hr class="hr15-top"> `;
-				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 5000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 			} 
 
-			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
+			setTimeout(() => { $('#exampleModal').modal('hide'); }, 4000);
 
-			setTimeout(() => { generatePDF(); }, 7000);
+			setTimeout(() => { generatePDF(); }, 6000);
 
 			var docRef = db.collection("users").doc(theGuy);
 			docRef.get().then((doc) => { 
