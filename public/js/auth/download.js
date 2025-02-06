34635af6d947fffcc4c272b049f0bd505651da9a
Joe <joemunene91@gmail.com>
2025-02-06 00:14:15 -0800

    h

--- public/js/auth/download.js
+++ public/js/auth/download.js
@@ -124,23 +124,23 @@ auth.onAuthStateChanged(user => {
 					var shortCutFunction = 'success'; var msg = ` 
 						Logins will be sent to <br> ${user.email}                     <hr class="to-hr hr15-top"> 
 						Verify your email inbox,  <br> Check the spam - folder.       <hr class="hr15-top"> `;
-					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 5500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
+					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;					
 				} else { 
 					var shortCutFunction = 'success';  var msg = ` 
 						${toastbtci} BTC not detected <br> Send exactly $${toastzi}.  <hr class="to-hr hr15-top"> 
 						Bank logs will be sent to <br> ${user.email}.                 <hr class="hr15-top"> `;
-					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 5500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+					toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 				}});
 			} else {
 				var shortCutFunction = 'success';  var msg = ` 
 					${toastbtci} BTC not detected <br> Send exactly $${toastzi}.   <hr class="to-hr hr15-top"> 
 					You're not logged in <br> with an email address ..             <hr class="hr15-top"> `;
-				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 5500, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
+				toastr.options =  {closeButton: true, debug: false, newestOnTop: true, progressBar: true, timeOut: 6000, positionClass: 'toast-top-full-width', preventDuplicates: true, onclick: null}; var $toast = toastr[shortCutFunction](msg);$toastlast = $toast;
 			}
 
-			setTimeout(() => { $('#exampleModal').modal('hide'); }, 4500);
+			setTimeout(() => { $('#exampleModal').modal('hide'); }, 5000);
 
-			setTimeout(() => { generatePDF(); }, 6500);
+			setTimeout(() => { generatePDF(); }, 7000);
 
 			var docRef = db.collection("users").doc(theGuy);
 			docRef.get().then((doc) => { 
@@ -162,7 +162,7 @@ auth.onAuthStateChanged(user => {
 		setTimeout(() => { 
 			var pdfObject = jsPDFInvoiceTemplate.default(props); 
 			console.log("Object created", pdfObject); 
-		}, 4500);
+		}, 4000);
 	}
 
 	if(JSON.parse(nesh).length == 1) {
